5693a6614911e4cbc8eed088d6f1b59850f1041a
romeoscript <romeobourne211@gmail.com>
2024-12-10 10:27:11 +0100

    updated

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,5 @@
 
 import Hero from '../components/Hero'
-import CoreValues from '../components/CoreValues'
-import Products from '../components/Products'
-import Testimonials from '../components/Testimonials'
-import CTASection from '../components/CTASection'
 import SalesPage from '../components/Sales'
 
 const Homepage = () => {
